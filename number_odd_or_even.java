6d4a9ad80704e01a7b3534e61a1b2d0d9cb18de9
Peram Brahma <94387176+perambrahma123@users.noreply.github.com>
2023-09-16 15:06:54 +0530

    Create number_odd_or_even.java

---
+++ number_odd_or_even.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class Main{
+  public static void main(String [] args){
+    Scanner s=new Scanner(System.in); //creating scanning object 
+    int n=s.nextInt();//taking the input
+    if(n%2==0)//checking n is even or not 
+    {
+      System.out.println("Even"); //statement if true then it print even 
+    }
+    else{ //if not even then it automatically prints
+      System.out.println("Odd"); //if not even then it is odd so print odd
+    }
+    
+  }}
+    
