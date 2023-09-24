b73ea424164959b19757e90986e361972aec3599
Peram Brahma <94387176+perambrahma123@users.noreply.github.com>
2023-09-24 21:21:11 +0530

    Create Multiplication.java

---
+++ BODMAS/Multiplication.java
@@ -0,0 +1,23 @@
+//here the addition of integre,float and both is are shown 
+import java.util.*;
+public class Main
+{
+	public static void main(String[] args) {
+		//System.out.println("Hello World");
+		Scanner s=new Scanner(System.in);
+		System.out.println("enter two integers you want to multiple:");
+		int t=s.nextInt();
+	    int y=s.nextInt();
+	    System.out.println(t*y);
+	    System.out.println("enter two float you want to multiple:");
+        float a=s.nextFloat();
+        float b=s.nextFloat();
+        System.out.println(a*b);
+        
+        System.out.println("down is value of integer t and float b \n"+t*b);
+        
+
+		    
+		
+	}
+}
